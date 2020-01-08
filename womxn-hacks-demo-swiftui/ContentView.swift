fda867a2a556a2465165cafabd9d24043cba36d8
Scott Chow <tehchowster@gmail.com>
2020-01-08 13:33:57 -0800

    Initial Commit

---
+++ womxn-hacks-demo-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  womxn-hacks-demo-swiftui
+//
+//  Created by Scott P. Chow on 1/8/20.
+//  Copyright © 2020 scottpchow. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
